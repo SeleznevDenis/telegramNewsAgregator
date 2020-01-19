d12cf3d14287a626ecb0e7822a8700adcb644462
Denis Seleznev <d.selezneww@yandex.ru>
2020-01-19 21:20:10 +0500

    Сменил имя приложения

--- src/main/java/ru/dselezneww/TelegramNewsFeederApplication.java
+++ src/main/java/ru/dselezneww/TelegramNewsFeederApplication.java
@@ -3,11 +3,9 @@ package ru.dselezneww;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.dselezneww.models.NewsSources;
 
 @ComponentScan("ru.dselezneww")
 @EnableAutoConfiguration
